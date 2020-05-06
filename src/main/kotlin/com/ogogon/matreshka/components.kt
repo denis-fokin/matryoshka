f60277daa15495ff5f45e60e75ab0d71d8a8c607
saktush <sergey.aktush@gmail.com>
2020-05-06 18:15:34 +0300

    radio button added

--- src/main/kotlin/com/ogogon/matreshka/components.kt
+++ src/main/kotlin/com/ogogon/matreshka/components.kt
@@ -3,6 +3,7 @@ package com.ogogon.matreshka
 import javax.swing.JButton
 import javax.swing.JComboBox
 import javax.swing.JLabel
+import javax.swing.JRadioButton
 
 fun JButton(constraints:Any? = null, function: JButton.() -> Unit): JButton = JButton().apply {
     function.invoke(this)
@@ -15,6 +16,11 @@ fun JLabel(constraints:Any? = null, function: JLabel.() -> Unit): JLabel = JLabe
 }
 
 fun JComboBox(constraints:Any? = null, function: JComboBox<Any?>.() -> Unit): JComboBox<Any?> = JComboBox<Any?>().apply {
+    function.invoke(this)
+    lastParent?.add(this, constraints)
+}
+
+fun JRadioButton(constraints:Any? = null, function: JRadioButton.() -> Unit): JRadioButton = JRadioButton().apply {
     function.invoke(this)
     lastParent?.add(this, constraints)
 }
