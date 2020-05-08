98eebac240568564a94f58a1c4f7f351c508cc3b
Denis Fokin <denis.fokin@jetbrains.com>
2020-05-08 11:28:39 +0300

    Borders, alignments, ComboBoxDemo2

--- src/main/kotlin/com/ogogon/matreshka/demo/ComboBoxDemo2.kt
+++ src/main/kotlin/com/ogogon/matreshka/demo/ComboBoxDemo2.kt
@@ -2,9 +2,11 @@ package com.ogogon.matreshka.demo
 
 import com.ogogon.matreshka.*
 import java.awt.*
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.swing.*
 
-private val patternExamples = arrayOf<String?>(
+private val patternExamples = arrayOf<String>(
         "dd MMMMM yyyy",
         "dd.MM.yy",
         "MM/dd/yy",
@@ -19,28 +21,28 @@ private val patternExamples = arrayOf<String?>(
 var currentPattern = patternExamples[0]
 
 object ComboboxDemoController2 {
-    // This is the model
-    val petStrings = arrayOf<String?>("Bird", "Cat", "Dog", "Rabbit", "Pig")
-    var pictureLabel: JLabel? = null
-    var currentSelection: String? = null
+    private lateinit var result: JLabel
+    private val formatter
+        get () = SimpleDateFormat(currentPattern)
+    val dateString
+        get () = formatter.format(Date())
 
     fun plug(pl: JLabel) {
-        pictureLabel = pl
+        result = pl
     }
 
     fun update() {
-        val icon = createImageIcon("images/$currentSelection.gif")
-        pictureLabel?.icon = icon
-        pictureLabel?.toolTipText = "A drawing of a " + currentSelection?.toLowerCase()
-        if (icon != null) {
-            pictureLabel?.text = null
-        } else {
-            pictureLabel?.text = "Image not found"
+        try {
+            result.foreground = Color.black
+            result.text = dateString
+        } catch (iae: IllegalArgumentException) {
+            result.foreground = Color.red
+            result.text = "Error: " + iae.message
         }
     }
 
     fun updateCurrentSelection(s: String) {
-        currentSelection = s
+        currentPattern = s
     }
 
 }
@@ -49,42 +51,32 @@ fun main() {
 
     JFrame {
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        title = "ComboBox Demo"
+        title = "ComboBox Demo 2"
 
 
         // Content pane
         BoxLayout(axis = BoxLayout.PAGE_AXIS) {
-            //todo
-            //border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+            border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
             // Pattern pane
             BoxLayout(axis = BoxLayout.PAGE_AXIS) {
-                //todo
-                //panel.alignmentX = Component.LEFT_ALIGNMENT
+                alignmentX = Component.LEFT_ALIGNMENT
+
                 JLabel {
                     text = "Enter the pattern string or"
                 }
+
                 JLabel {
                     text = "select one from the list:"
                 }
 
-
                 JComboBox {
                     isEditable = true
                     alignmentX = Component.LEFT_ALIGNMENT
+                    model = DefaultComboBoxModel(patternExamples)
                     onAction {
-                       /* val cb = e.source as JComboBox<*>
-                        val newSelection = cb.selectedItem as String
-                        currentPattern = newSelection
-                        val today = Date()
-                        val formatter = SimpleDateFormat(currentPattern)
-                        try {
-                            val dateString = formatter.format(today)
-                            result.foreground = Color.black
-                            result.text = dateString
-                        } catch (iae: IllegalArgumentException) {
-                            result.foreground = Color.red
-                            result.text = "Error: " + iae.message
-                        }*/
+                        val cb = it.source as JComboBox<*>
+                        ComboboxDemoController2.updateCurrentSelection(cb.selectedItem as String)
+                        ComboboxDemoController2.update()
                     }
                 }
             }
@@ -96,22 +88,25 @@ fun main() {
                 columns = 1
                 rows = 0
 
-                //resultPanel.alignmentX = Component.LEFT_ALIGNMENT
+                alignmentX = Component.LEFT_ALIGNMENT
 
                 //ResultLabel label
                 JLabel {
                     text = "Current Date/Time"
                     horizontalAlignment = JLabel.LEADING
                 }
+
                 //Result label
                 JLabel {
-                    text = " "
                     foreground = Color.BLACK
                     border = BorderFactory.createCompoundBorder(
                             BorderFactory.createLineBorder(Color.black),
                             BorderFactory.createEmptyBorder(5, 5, 5, 5))
+                    apply {
+                        ComboboxDemoController2.plug(this)
+                        ComboboxDemoController2.update()
+                    }
                 }
-
             }
         }
     }
