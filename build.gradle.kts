611bc41c9d737302eb6db8a41b2eaedcbe38ec6c
Razmik Seysyan <razmik.seysyan@jetbrains.com>
2020-05-25 13:13:22 +0200

    Basic setup for maven and bintray publications

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
     id("maven-publish")
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "com.ogogon.swing.dsl"
@@ -20,7 +21,50 @@ tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+
+val publicationName = "Lib" // could by any
+
+publishing {
+    publications {
+        create<MavenPublication>(publicationName) {
+            from(project.components["java"])
+        }
+    }
+
+    repositories {
+        // TODO: target repo for publish otherwise only publishToMavenLocal
+    }
+}
+
+bintray {
+    user = when {
+        project.hasProperty("bintrayUser") -> project.property("bintrayUser").toString()
+        else -> System.getenv("BINTRAY_USER")
+    }
+
+    key = when {
+        project.hasProperty("bintrayApiKey") -> project.property("bintrayApiKey").toString()
+        else -> System.getenv("BINTRAY_API_KEY")
+    }
+
+    publish = true
+
+    pkg.apply {
+        repo = "" // TODO: Provide target repo name
+
+        vcsUrl = "https://github.com/denis-fokin/matreshka"
+        setLicenses("Apache-2.0")
+
+        name = "matreshka"
+        version.apply {
+            name = "0.0.0" //TODO: pass from CI or somewhere else
+        }
+    }
+
+    setPublications(publicationName)
 }
