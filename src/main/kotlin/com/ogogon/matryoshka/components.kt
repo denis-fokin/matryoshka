a10544d378abbf1486ae12637ec17927ff67653a
Denis Fokin <denis.fokin@jetbrains.com>
2020-05-31 00:44:21 +0300

    More JComponents (Part I)

--- src/main/kotlin/com/ogogon/matryoshka/components.kt
+++ src/main/kotlin/com/ogogon/matryoshka/components.kt
@@ -1,31 +1,70 @@
 package com.ogogon.matryoshka
 
+import java.awt.FlowLayout
 import javax.swing.*
 
-fun JButton(constraints:Any? = null, function: JButton.() -> Unit): JButton = JButton().apply {
+/*
+AbstractButton,
+BasicInternalFrameTitlePane,
+Box,
+Box.Filler,
+JColorChooser,
+JComboBox,
+JFileChooser,
+JInternalFrame,
+JInternalFrame.JDesktopIcon,
+JLabel,
+JLayer,
+JLayeredPane,
+JList,
+JMenuBar,
+JOptionPane,
+JPanel,
+JPopupMenu,
+JProgressBar,
+JRootPane,
+JScrollBar,
+JScrollPane,
+JSeparator,
+JSlider,
+JSpinner,
+JSplitPane,
+JTabbedPane,
+JTable,
+JTableHeader,
+JTextComponent,
+JToolBar,
+JToolTip,
+JTree,
+JViewport
+ */
+
+/*
+JButton.java
+JCheckBox.java
+JCheckBoxMenuItem.java
+JMenu
+JMenu.java
+JMenuItem.java
+JPopupMenu
+JRadioButton.java
+JRadioButtonMenuItem.java
+JTable
+JToggleButton.java
+JToolBar
+ */
+
+inline fun <reified T: JComponent> Komponent (constraints:Any? = null, function: T.() -> Unit) : T
+        = T::class.java.getDeclaredConstructor().newInstance().apply {
     function.invoke(this)
     lastParent?.add(this, constraints)
 }
 
-fun JLabel(constraints:Any? = null, function: JLabel.() -> Unit): JLabel = JLabel().apply {
-    function.invoke(this)
-    lastParent?.add(this, constraints)
-}
-
-fun JTextField(constraints:Any? = null, function: JTextField.() -> Unit): JTextField = JTextField().apply {
-    function.invoke(this)
-    lastParent?.add(this, constraints)
-}
-
-fun JTextArea(constraints:Any? = null, function: JTextArea.() -> Unit): JTextArea = JTextArea().apply {
-    function.invoke(this)
-    lastParent?.add(this, constraints)
-}
-
-fun JComboBox(constraints:Any? = null, function: JComboBox<Any?>.() -> Unit): JComboBox<Any?> = JComboBox<Any?>().apply {
-    function.invoke(this)
-    lastParent?.add(this, constraints)
-}
+fun JButton(c:Any? = null, f: JButton.() -> Unit) = Komponent(c, f)
+fun JLabel(c:Any? = null, f: JLabel.() -> Unit) = Komponent(c, f)
+fun JTextField(c:Any? = null, f: JTextField.() -> Unit) = Komponent(c, f)
+fun JTextArea(c:Any? = null, f: JTextArea.() -> Unit) = Komponent(c, f)
+fun JComboBox(c:Any? = null, f: JComboBox<Any?>.() -> Unit) = Komponent(c, f)
 
 fun JRadioButton(constraints:Any? = null, function: JRadioButton.() -> Unit): JRadioButton = JRadioButton().apply {
     lastParent?.addToButtonGroup(this)
